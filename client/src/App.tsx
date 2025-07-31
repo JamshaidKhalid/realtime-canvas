bb097cee182b7e949b846c39d4a58c8968277cca
jamshaidkhalid <jamshaidkhalid32@gmail.com>
2025-07-31 23:52:10 +0500

    chore: react and tailwind setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-white">Hello</h1>
+    </div>
+      </>
+  )
+}
+
+export default App
